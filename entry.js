cceb8ada869c9c50712e38044c1d8279f58f94dc
TakesxiSximada <takesxi.sximada@gmail.com>
2015-05-31 14:07:04 +0900

    WIP

--- entry.js
+++ entry.js
@@ -3,4 +3,7 @@ require('./bower_components/foundation/css/foundation.css');
 require('./bower_components/foundation/css/foundation.css');
 require('./bower_components/foundation/js/vendor/modernizr.js');
 require('./bower_components/foundation/js/vendor/modernizr.js');
+
+require('./bower_components/font-awesome/css/font-awesome.css');
+
 // require('./bower_components/d3/d3.min.js');
